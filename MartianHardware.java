905a794ab5658c3eec5401a3641b250e61684cce
pjayamoh <pjayamoh@users.noreply.github.com>
2018-11-15 21:41:19 -0500

    Initial Checkin

---
+++ MartianHardware.java
@@ -0,0 +1,205 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a Pushbot.
+ * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Left  drive motor:        "left_drive"
+ * Motor channel:  Right drive motor:        "right_drive"
+ * Motor channel:  Manipulator drive motor:  "left_arm"
+ * Servo channel:  Servo to open left claw:  "left_hand"
+ * Servo channel:  Servo to open right claw: "right_hand"
+ */
+public class MartianHardware
+{
+    /* Public OpMode members. */
+    public DcMotor  rearLeftDrive   = null;
+    public DcMotor  rearRightDrive  = null;
+    public DcMotor  frontLeftDrive   = null;
+    public DcMotor  frontRightDrive  = null;
+    
+    public DcMotor  motorArmLeft     = null;
+    public DcMotor  motorArmRight     = null;
+    
+    public Servo    leftClaw    = null;
+    public Servo    rightClaw   = null;
+    public Servo    contLinear  = null;
+    
+    public Servo    sensorArm   = null;
+    
+    public DistanceSensor sensorRange = null;
+    public ColorSensor sensorColor = null;
+
+    public static final double MID_SERVO       =  0.45 ;
+    public static final double ARM_UP_POWER    =  0.1 ;
+    public static final double ARM_DOWN_POWER  = -0.1;
+   
+
+
+    /* Vuforia Variables */
+    public static final String TAG = "Vuforia VuMark Martians";
+    public OpenGLMatrix lastLocation = null;
+    public VuforiaLocalizer vuforia = null;
+    public VuforiaLocalizer.Parameters parameters = null;
+    public VuforiaTrackables relicTrackables = null;
+    public VuforiaTrackable relicTemplate = null;
+    public RelicRecoveryVuMark vuMark = null;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public MartianHardware(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        /*
+         * To start up Vuforia, tell it the view that we wish to use for camera monitor (on the RC phone);
+         * If no camera monitor is desired, use the parameterless constructor instead (commented out below).
+         */
+        //int cameraMonitorViewId = hwMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hwMap.appContext.getPackageName());
+        //VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        // OR...  Do Not Activate the Camera Monitor View, to save power
+        //parameters = new VuforiaLocalizer.Parameters();
+        
+        //parameters.vuforiaLicenseKey = "AbCuv8X/////AAAAGf9WW1xLgEveg2n/MGV4F9pniTB4OvNJyhAd4UfqO7GF2/9tmJ+iLqqBSF1eCbql5kL/X+X5kKSEGK+kLQT298wVsRgWxA8o7UEZAIbJf85mI9s4WFpG3ZBYPU/EOGMFL0jYjkzS88kFHHKvT/mNwnJ5odymxzNihE92V2V/lCcspxOpccIVPo7nIoj//66jccfjFUfwcLLzWYGMGRO0PoOdI1lsUw7ALiWrfoyyFu1tM+UYTL1d+fmWd+MDV9gFOk13B/FD68SRxQPedAggg9JZY3h1be7gQsF4ybRRkdBIiM/MFv95blSOd9OKPgv6bf7c5lZiDUTgf25ohh+TdE2oYKTpi4iaZtSUcVvX5Qep";
+
+        /*
+         * We also indicate which camera on the RC that we wish to use.
+         * Here we chose the back (HiRes) camera (for greater range), but
+         * for a competition robot, the front camera might be more convenient.
+         */
+        //parameters.cameraDirection = VuforiaLocalizer.CameraDirection.FRONT;
+        //this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        /**
+         * Load the data set containing the VuMarks for Relic Recovery. There's only one trackable
+         * in this data set: all three of the VuMarks in the game were created from this one template,
+         * but differ in their instance id information.
+         * @see VuMarkInstanceId
+         */
+        //relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        //relicTemplate = relicTrackables.get(0);
+        //relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+
+        // Define and Initialize Motors
+        rearLeftDrive  = hwMap.get(DcMotor.class, "motorRearLeft");
+        rearRightDrive = hwMap.get(DcMotor.class, "motorRearRight");
+        frontLeftDrive  = hwMap.get(DcMotor.class, "motorFrontLeft");
+        frontRightDrive = hwMap.get(DcMotor.class, "motorFrontRight");
+        
+        motorArmLeft    = hwMap.get(DcMotor.class, "motorArmLeft");
+        motorArmRight    = hwMap.get(DcMotor.class, "motorArmRight");
+        
+        rearLeftDrive.setDirection(DcMotor.Direction.FORWARD); 
+        rearRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD); 
+        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Set all motors to zero power
+        rearLeftDrive.setPower(0); 
+        rearRightDrive.setPower(0);
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0);
+
+        motorArmLeft.setPower(0);
+        motorArmRight.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        
+        rearLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rearRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        motorArmLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorArmRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Define and initialize all installed servos.
+        leftClaw  = hwMap.get(Servo.class, "servoLeftClaw");
+        rightClaw = hwMap.get(Servo.class, "servoRightClaw");
+        contLinear = hwMap.get(Servo.class, "servoContLinear");
+        
+        //sensorArm = hwMap.get(Servo.class, "servoSensorArm");
+        
+        //leftClaw.setPosition(MID_SERVO);
+        //rightClaw.setPosition(MID_SERVO);
+        //sensorArm.setPosition(MID_SERVO);
+        
+        // Define and initialize all Sensors
+        //sensorRange = hwMap.get(DistanceSensor.class, "sensorColorRange");
+        //sensorColor = hwMap.get(ColorSensor.class, "sensorColorRange");
+        
+    }
+ }
---
+++ MartianTeleOpScrimmage.java
@@ -0,0 +1,271 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.hardware.DigitalChannelImpl;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Martian TeleOp Scrimmage", group="Linear Opmode")  // @Autonomous(...) is the other common choice
+
+public class MartianTeleOpScrimmage extends LinearOpMode {
+    //private Gyroscope imu;
+    private DcMotor motorRearLeft, motorRearRight, motorFrontLeft, motorFrontRight;
+    private DcMotor motorArmLeft, motorArmRight;
+    
+    private Servo servoLeftClaw, servoRightClaw, servoContLinear;
+    
+    private DigitalChannel digitalTouch;
+    private DistanceSensor sensorColorRange;
+    private ColorSensor sensorColor;
+    public Servo    sensorArm   = null;
+    private double dblSpeedMultiplier = 0.7;  
+    private double dblTurnSpeed = 0.7;
+    
+
+
+    @Override
+    public void runOpMode() {
+        
+        /* Initialize wheel motors */
+        motorRearRight = hardwareMap.get(DcMotor.class, "motorRearRight");
+        motorRearLeft = hardwareMap.get(DcMotor.class, "motorRearLeft");
+        motorFrontRight = hardwareMap.get(DcMotor.class, "motorFrontRight");
+        motorFrontLeft = hardwareMap.get(DcMotor.class, "motorFrontLeft");
+        
+        /* Initialize Arm motors */
+        motorArmLeft = hardwareMap.get(DcMotor.class, "motorArmLeft");
+        motorArmRight = hardwareMap.get(DcMotor.class, "motorArmRight");
+        
+        /* Initialize Servo motors */
+        servoLeftClaw = hardwareMap.get(Servo.class, "servoLeftClaw");
+        servoRightClaw = hardwareMap.get(Servo.class, "servoRightClaw");
+        servoContLinear = hardwareMap.get(Servo.class, "servoContLinear");
+      
+        //digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+        //sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+        //sensorColor = hardwareMap.get(ColorSensor.class, "sensorColorRange");
+        //sensorArm = hardwareMap.get(Servo.class, "servoSensorArm");
+
+        // Set digital channel to input Mode
+        //digitalTouch.setMode(DigitalChannel.Mode.INPUT);
+        
+        //telemetry.addData("Status", "Initialized");
+        //telemetry.update();
+        
+        //sensorArm.setPosition(0.50);
+            
+        
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        
+        double tgtPowerRearLeft = 0;
+        double tgtPowerRearRight = 0;
+        double tgtPowerFrontLeft = 0;
+        double tgtPowerFrontRight = 0;
+        
+        double tgtPowerRightStickX = 0;
+        
+        
+        boolean tgtPowerRightBumper = false;
+        boolean tgtPowerLeftBumper = false;
+        
+        double tgtPowerArm = 0;
+        
+
+        while (opModeIsActive()) {
+            
+            // Set the controls for Gamepad 1
+            tgtPowerFrontLeft = this.gamepad1.left_stick_y;
+            tgtPowerRearLeft = this.gamepad1.left_stick_y;
+            tgtPowerFrontRight = -this.gamepad1.left_stick_y;
+            tgtPowerRearRight = -this.gamepad1.left_stick_y;
+            
+            //tgtPowerFrontLeft = this.gamepad1.right_stick_x;
+            //tgtPowerRearLeft = this.gamepad1.right_stick_x;
+            
+            //tgtPowerFrontRight = -this.gamepad1.right_stick_x;
+            //tgtPowerRearRight = -this.gamepad1.right_stick_x;
+            
+            tgtPowerRightStickX = this.gamepad1.right_stick_x;
+            
+            
+            tgtPowerRightBumper = this.gamepad1.right_bumper;
+            tgtPowerLeftBumper = this.gamepad1.left_bumper;
+            
+            // Set Controls for Gamepad 1
+            //if (gamepad1.a) {
+            //    dblSpeedMultiplier = 0.6;
+            //} else if (gamepad1.b) {
+            //    dblSpeedMultiplier = 1.0;
+            //}
+            
+            // Check for right or left bumber and Straf if pressed
+            
+            if (tgtPowerLeftBumper) {
+                motorRearLeft.setPower(-1);
+                motorRearRight.setPower(-1);
+                motorFrontLeft.setPower(1);
+                motorFrontRight.setPower(1);
+            }
+            else if (tgtPowerRightBumper) {
+                motorRearLeft.setPower(1);
+                motorRearRight.setPower(1);
+                motorFrontLeft.setPower(-1);
+                motorFrontRight.setPower(-1);
+            }
+            else if (tgtPowerRightStickX != 0) {
+                
+                motorRearLeft.setPower(tgtPowerRightStickX*-dblTurnSpeed);
+                motorFrontLeft.setPower(tgtPowerRightStickX*-dblTurnSpeed);
+                motorRearRight.setPower(tgtPowerRightStickX*-dblTurnSpeed);
+                motorFrontRight.setPower(tgtPowerRightStickX*-dblTurnSpeed);
+            }
+          
+            else {
+                motorRearLeft.setPower(tgtPowerRearLeft*dblSpeedMultiplier);
+                motorRearRight.setPower(tgtPowerRearRight*dblSpeedMultiplier);
+                motorFrontLeft.setPower(tgtPowerFrontLeft*dblSpeedMultiplier);
+                motorFrontRight.setPower(tgtPowerFrontRight*dblSpeedMultiplier);
+            } 
+            
+            //telemetry.addData("Front Left Target Power", tgtPowerFrontLeft);
+            //telemetry.addData("Motor Power", motorFrontLeft.getPower());
+            
+            //telemetry.addData("Rear Left Target Power", tgtPowerRearLeft);
+            //telemetry.addData("Motor Power", motorRearLeft.getPower());
+            
+            //telemetry.addData("Front Right Target Power", tgtPowerFrontRight);
+            //telemetry.addData("Motor Power", motorFrontRight.getPower());
+            
+            //telemetry.addData("Rear Right Target Power", tgtPowerRearRight);
+            //telemetry.addData("Motor Power", motorRearRight.getPower());
+            
+            // Set the controls for Gamepad 2
+            tgtPowerArm = this.gamepad2.right_stick_y;
+            
+            if (this.gamepad2.right_stick_y == 0) {
+            motorArmLeft.setPower(0);
+            motorArmRight.setPower(0);
+            motorArmLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            motorArmRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            }
+            else {
+            motorArmLeft.setPower(tgtPowerArm*0.3);
+            //motorArmLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            
+            motorArmRight.setPower(-tgtPowerArm*0.3);
+            //motorArmRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            }
+            // Check to see if you need to move Servo
+            if (gamepad2.b) {
+                //move to 0 degree
+                servoLeftClaw.setPosition(0.3);
+                servoRightClaw.setPosition(0.7);
+            } else if (gamepad2.a) {
+                //move to 180 degrees
+                servoLeftClaw.setPosition(.95);
+                servoRightClaw.setPosition(.05);
+            }
+            
+            if (gamepad2.right_bumper) {
+                //Move the arm up
+                servoContLinear.setPosition(1);
+                
+            } else if (gamepad2.left_bumper) {
+                //Move the arm down
+                servoContLinear.setPosition(0);
+                
+            } else {
+                servoContLinear.setPosition(0.5);
+            //motorArmLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            }
+            
+            // digitalTouch - Check if the button was pressed
+            //if (digitalTouch.getState() == false) {
+            //    // Button is pressed
+            //    telemetry.addData("Button", "PRESSED");
+            //} else {
+            //    // Button is not pressed
+            //    telemetry.addData("Button", "NOT PRESSED");
+            //}
+            
+            // Color Sensor
+            //if (sensorColor.red() < 25 && sensorColor.blue() < 25 ) {
+            //    telemetry.addData("Color", "UNKNOWN");
+            //} else if (sensorColor.red() < sensorColor.blue() ) {
+            //    telemetry.addData("Color", "BLUE");
+            //} else {
+            //    telemetry.addData("Color", "RED");
+            //}
+            
+            //telemetry.addData("servoLeftClaw", servoLeftClaw.getPosition());
+            //telemetry.addData("servoRightClaw", servoRightClaw.getPosition());
+            
+            //telemetry.addData("Distance (cm)", sensorColorRange.getDistance(DistanceUnit.CM));
+            
+            //telemetry.addData("Color I2cAdress", sensorColor.getI2cAddress());
+            //telemetry.addData("Color Red", sensorColor.red());
+            //telemetry.addData("Color Blue", sensorColor.blue());
+            
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+        
+        }
+    }
+}
